d0bdb2eed713ec4b0c16706b89b3cb4437754fbf
otaviolage <otaviolagedossantos@gmail.com>
2022-01-15 11:55:35 -0300

    Updates

--- html.js
+++ html.js
@@ -14,7 +14,7 @@ export default function HTML({
   body,
 }) {
   return (
-    <html {...htmlAttributes} basename={process.env.PUBLIC_URL}>
+    <html {...htmlAttributes} basename={'https://otaviolage.github.io/mentorinc/'}>
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
